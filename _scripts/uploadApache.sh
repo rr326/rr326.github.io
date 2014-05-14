00df5966dab0ef27dc9de96fd24bbf2bb218b858
Ross Rosen <rrosen326@gmail.com>
2014-05-14 06:26:56 -0700

    making zephyrzone work

--- _scripts/uploadApache.sh
+++ _scripts/uploadApache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 rsync  --rsh=ssh -aizhL  ./apache/k2company.conf rrosen326@prod2.weather-explorer.com:/etc/apache2/sites-available/
+rsync  --rsh=ssh -aizhL  ./apache/zephyrzone.conf rrosen326@prod2.weather-explorer.com:/etc/apache2/sites-available/
